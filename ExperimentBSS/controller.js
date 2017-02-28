75ef5cc8903bca6ca945442dcc50b53bd235678a
fricer <yovchev23@gmail.com>
2017-02-28 18:21:14 +0100

    Added visualization of the stimuli on the screen

--- ExperimentBSS/controller.js
+++ ExperimentBSS/controller.js
@@ -1,8 +1,12 @@
-﻿var timeOut = 40;
+﻿var loadingTime = 4000;
+var timeOut = 750;
+
 var stimulusLength = 3;
 var digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var symbols = ['+', '$', '%', '!', '#', '&', '@', '?', '*', '~'];
+
+var timer = 0;
 
 $(".group_button").click(function () {
     var group = $(this).val();
@@ -14,18 +18,19 @@ $(".group_button").click(function () {
     // prepare participants for the visual stimuli
     showLoadingBar();
 
-    // prep canvas for the upcoming visual stimuli
-    setTimeout(prepCanvasForStimuli, timeOut + 100);
-
     // generate visual stimuli per group
     var stimuli = generateRandomStringForGroup(group);
     console.log(stimuli);
+
+    // prep canvas for the upcoming visual stimuli
+    setTimeout(prepCanvasForStimuli, loadingTime);
+    setTimeout(showVisualStimuli, loadingTime, stimuli);
 });
 
 function showLoadingBar() {
     var bar = new ProgressBar.Circle('#container', {
         easing: 'linear',
-        duration: timeOut,
+        duration: loadingTime,
         color: '#3498db',
         trailColor: '#d3d3d3',
         strokeWidth: 6,
@@ -55,7 +60,29 @@ function showLoadingBar() {
 
 function prepCanvasForStimuli() {
     $("#container").hide();
-    $("#text_window").css("font-size", "30vmax").text('H').show();
+}
+
+function showVisualStimuli(string) {
+    var textwindow = $("#text_window");
+    textwindow.css("font-size", "30vmax").text(' ').show();
+
+    for (var i = 0; i < 2 * string.length; i++) {
+        presentSymbol(textwindow, string, i);
+    }
+}
+
+function presentSymbol(window, string, i) {
+    setTimeout(function () {
+        if (timer % 2 == 0) {
+            window.text(string[parseInt(timer/2)]);
+        }
+        else {
+
+            window.text(' ');
+        }
+
+        timer++;
+    }, (i + 1) * timeOut);
 }
 
 function generateRandomStringForGroup(group) {
